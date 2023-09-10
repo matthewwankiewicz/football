a56e760cf6372a9ea214a1fca18e724659ade352
Matthew Wankiewicz <matthew.wankiewicz@hotmail.com>
2023-09-09 21:25:15 -0400

    updated and deployed shiny app

--- football_plots/app.R
+++ football_plots/app.R
@@ -39,8 +39,7 @@ ui <- navbarPage("Fantasy Football Data",
     # Sidebar with a slider input for number of bins 
     tabPanel("Season Stats", 
              selectInput("position", label = "Select a position:",
-                         choices = c("QB", "RB", "WR", "TE"),
-                         ),
+                         choices = c("QB", "RB", "WR", "TE")),
              checkboxInput("avgs", "Per game stats"),
              
              
@@ -57,7 +56,15 @@ ui <- navbarPage("Fantasy Football Data",
                                      "receiving_yards", "rushing_yards", "targets",
                                      "target_share", "carries", "passing_yards", "air_yards_share",
                                      "rushing_tds", "receiving_tds", "passing_tds")),
-    plotlyOutput("weekly_plot"))
+             numericInput("prop", "Enter a threshold:", 0.5, step = 1),
+    plotlyOutput("weekly_plot")),
+    tabPanel("Team Graph",
+             selectInput("team_stat", "Select a stat to group by:",
+                         choices = c("fantasy_points_half_ppr", "receptions", "interceptions",
+                                     "receiving_yards", "rushing_yards", "targets",
+                                     "target_share", "carries", "passing_yards", "air_yards_share",
+                                     "rushing_tds", "receiving_tds", "passing_tds")),
+    plotlyOutput("team_plot"))
 )
 
 
@@ -144,16 +151,43 @@ server <- function(input, output) {
       filter(player_display_name == input$player_graph) %>% 
       select(input$stat_choice, week)
     
+    threshold_value <- input$prop
     
     plot_col <- colnames(filtered_data)[1]
     
+    # Create a new column for color based on the threshold
+    filtered_data$Result <- ifelse(filtered_data[[plot_col]] > threshold_value, "Over", "Under")
+    
+    
     plot <- filtered_data %>% 
-      ggplot(aes(x = week, y = !!sym(plot_col))) +
+      ggplot(aes(x = week, y = !!sym(plot_col), fill = Result)) +
       geom_col() +
-      labs(title = paste(input$player_graph, plot_col))
+      scale_fill_manual(values = c("Over" = "#61db40", "Under" = "#e86e4d")) +
+      labs(title = paste(input$player_graph, " - ", str_replace_all(plot_col, "_", " ")),
+           y = str_replace_all(plot_col, "_", " ")) +
+      theme_minimal() +
+      geom_hline(yintercept = input$prop)
     
     ggplotly(plot)
     
+  })
+  
+  output$team_plot <- renderPlotly({
+    
+    teams <- game_logs %>% filter(player_display_name == input$player_graph) %>% pull(recent_team)
+    team <- teams[length(teams)]
+
+    focus_stat <- input$team_stat
+
+    plot <- game_logs %>%
+      group_by(recent_team, week, player_display_name) %>%
+      summarise(stat = mean(!!sym(focus_stat))) %>%
+      filter(recent_team == team) %>% 
+      ggplot(aes(week, stat, color = player_display_name)) +
+      geom_line() +
+      theme_minimal()
+
+    ggplotly(plot)
   })
 
 }
